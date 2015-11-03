20ed5c33ae9dff1ea137385a095261333e695bc0
Hiroki Kondo <kompiro@gmail.com>
2015-11-03 19:07:12 +0900

    Sync the setting to webpack.production.js

--- webpack.production.js
+++ webpack.production.js
@@ -2,11 +2,14 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var devFlagPlugin = new webpack.DefinePlugin({
+  __QIITA_TOKEN: JSON.stringify(process.env.QIITA_TOKEN),
+  __QIITA_ENDPOINT: JSON.stringify(process.env.QIITA_ENDPOINT),
   __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
 });
 
 module.exports = {
   entry: [
+    'bootstrap-sass!./bootstrap-sass.config.js',
     './js/index.js'
   ],
   output: {
@@ -22,7 +25,14 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel'], exclude: [/node_modules/,/learning/] },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader?module!cssnext-loader') }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader!cssnext-loader') },
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.scss$/, loader: "style!css!sass?outputStyle=expanded" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
     ]
   },
   resolve: {
