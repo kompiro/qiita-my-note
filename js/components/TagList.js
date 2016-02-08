c95f8906fc70a9b203e402531dc80281aefb899c
Hiroki Kondo <kompiro@gmail.com>
2016-02-09 00:10:29 +0900

    Resolve style

--- js/components/TagList.js
+++ js/components/TagList.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 
 class TagList extends Component {
   render() {
     const {tags} = this.props;
     const tagNodes = tags.map((tag)=>{
-      let tagUrl = `${__QIITA_ENDPOINT}/${tag.name}`;
+      const tagUrl = `${__QIITA_ENDPOINT}/${tag.name}`;
       return (
         <div className="tag" key={tag.name}><a href={tagUrl} className="tagBase"><span className="tagName">{tag.name}</span></a></div>
-      )
+      );
     });
     return (
       <div className="tags">
         {tagNodes}
       </div>
-    )
+    );
   }
 }
 
-export default TagList
+TagList.propTypes = {
+  tags: React.PropTypes.array,
+};
+
+export default TagList;
